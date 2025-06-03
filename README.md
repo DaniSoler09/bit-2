77775f847fef409ae183171e73a4170de8762973
DaniSoler09 <soler092811@gmail.com>
2025-06-03 13:39:16 -0500

    updated

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-## BIT-2
+## Proyecto .json acerca de estudiantes BIT
+
+### Descripcion.
+
+Esta pagina busca implementar el conocimiento adquirido durante el proceso haciendo uso de tecnologias como HTML - CSS - JavaScript con la finalidad de mostrar la informacion de los estudiantes que se encuentra el el archivo .json. 
+
+- [GitHub](https://github.com/DaniSoler09/bit-2)
+
+### Autor
+
+- Daniel Ramirez Soler. 
--- js/script.js
+++ js/script.js
@@ -37,11 +37,31 @@ document.getElementById("next").addEventListener("click", function(){
     document.getElementById("projects").innerHTML = globalInfo[siguiente].projects[0].name + " / " + globalInfo[0].projects[1].name
 
     if (globalInfo[siguiente].projects[0].score[0] < 3.5){
-        document.getElementById("titulo").innerHTML = "FALLEN ONES"
+        document.getElementById("titulo").innerHTML = "FALLEN"
     }else if (globalInfo[siguiente].intensity === "100 hours") {
-        document.getElementById("titulo").innerHTML = "FALLEN ONES"
+        document.getElementById("titulo").innerHTML = "FALLEN"
     }else { 
-        document.getElementById("titulo").innerHTML = "SURVIVERS"
+        document.getElementById("titulo").innerHTML = "SURVIVER"
+    }
+  });
+
+  document.getElementById("back").addEventListener("click", function(){
+    siguiente--
+    document.getElementById("picStudent").src = "https://github.com/" + globalInfo[siguiente].usernameGithub + ".png";
+
+    document.getElementById("student").innerHTML = globalInfo[siguiente].student
+    document.getElementById("intensity").innerHTML = globalInfo[siguiente].intensity
+    document.getElementById("code").innerHTML = globalInfo[siguiente].code
+    document.getElementById("git").href = "https://github.com/" + globalInfo[siguiente].usernameGithub
+     document.getElementById("git").innerHTML = "https://github.com/" + globalInfo[siguiente].usernameGithub
+    document.getElementById("projects").innerHTML = globalInfo[siguiente].projects[0].name + " / " + globalInfo[0].projects[1].name
+
+    if (globalInfo[siguiente].projects[0].score[0] < 3.5){
+        document.getElementById("titulo").innerHTML = "FALLEN"
+    }else if (globalInfo[siguiente].intensity === "100 hours") {
+        document.getElementById("titulo").innerHTML = "FALLEN"
+    }else { 
+        document.getElementById("titulo").innerHTML = "SURVIVER"
     }
   });
 
